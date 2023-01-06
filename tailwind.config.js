23748ed199f9c2137549eb62f8024c0f859e81b9
Nikhil Rana <nikhilrana884@gmail.com>
2023-01-06 19:27:13 +0530

    tailwindcss configured

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    content: [
+        './screens/**/*.{html,js,jsx,ts,tsx}',
+      './pages/**/*.{html,js,jsx,ts,tsx}',
+      './components/**/*.{html,js,jsx,ts,tsx}',
+    ],
+    // ...
+};
