49e204fa07decd446aa959c4136aee7a4b0ecb32
Nikhil Rana <nikhilrana884@gmail.com>
2023-01-09 14:24:39 +0530

    Update HomeScreen.js

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,38 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+import React, { useEffect, useLayoutEffect } from 'react'
+import { View, Text, SafeAreaView, Image } from 'react-native'
 
 function HomeScreen() {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+      navigation.setOptions({
+        headerShown: false,
+      })
+  }, [])
+
+
   return (
-    <View>
-    <Text>HomeScreen</Text>
-    </View>
+    <SafeAreaView>
+      <Text>
+        <View>
+          <Image 
+          source = {{
+            uri: 'link ',
+          }}
+          className = "h-6 w-6 bg-white-200 p-3"
+          />
+        </View>
+        <View>
+          <Text>
+            Deliver Now 
+          </Text>
+          <Text>
+            Current Location
+          </Text>
+        </View>
+      </Text>
+    </SafeAreaView>
   )
 }
 
